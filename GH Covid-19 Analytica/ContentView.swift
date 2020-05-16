260e23d046e28693a5d5689eb6346abc31ffdbc2
AbdulKarim <Abdul.moro4u@gmail.com>
2020-05-16 20:27:54 +0900

    Initial Commit

---
+++ GH Covid-19 Analytica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GH Covid-19 Analytica
+//
+//  Created by Malori Morow on 2020/05/16.
+//  Copyright © 2020 Malori Morow. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
